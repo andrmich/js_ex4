b5d80693e18da69b2389cbd7adb38ab88cce784d
andrmich <47849025+andrmich@users.noreply.github.com>
2019-06-10 14:36:43 +0200

    added test to refactor_me

--- src/refactor_me.test.js
+++ src/refactor_me.test.js
@@ -1,1 +1,35 @@
 import * as rfm from "./refactor_me";
+
+beforeEach(() => {
+  rfm.Fruit.mockClear();
+});
+
+it("class can be called", () => {
+  const fruit = new rfm.Fruit("apple", "red", 12);
+  expect(fruit.type).toBe("apple");
+});
+
+test("get info about the fruit", () => {
+  const fruit = new rfm.Fruit("apple", "red", 12);
+  expect(fruit.getInfo()).toBe("This is red apple");
+});
+
+test("slice the fruit", () => {
+  const fruit = new rfm.Fruit("apple", "red", 12);
+  expect(fruit.slice()).toBe(6);
+});
+
+test("chop the fruit", () => {
+  const fruit = new rfm.Fruit("apple", "red", 12);
+  expect(fruit.chop(4)).toBe(3);
+});
+
+test("check if it is an apple", () => {
+  const fruit = new rfm.Fruit("apple", "red", 12);
+  expect(fruit.isThisApple()).toBe(true);
+});
+
+test("check if it is rotten", () => {
+  const fruit = new rfm.Fruit("apple", "red", 12);
+  expect(fruit.isRotten()).toBe(false);
+});
