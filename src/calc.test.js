1d0a55ec7b6a5a400dee36459ea7e8a599897ca7
andrmich <47849025+andrmich@users.noreply.github.com>
2019-06-11 08:44:13 +0200

    Update calc.test.js

--- src/calc.test.js
+++ src/calc.test.js
@@ -1,1 +1,15 @@
 import * as calc from "./calc";
+
+beforeEach(() => {
+  calc.Calc.mockClear();
+});
+
+test("for '' returns 0", () => {
+  const cal = new calc.Calc();
+  expect(cal.add("")).toBe(0);
+});
+
+test("for '1' returns 1", () => {
+  const cal = new calc.Calc();
+  expect(cal.add("1")).toBe(1);
+});
